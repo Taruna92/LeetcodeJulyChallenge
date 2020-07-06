bcadde52a7ccf7f484a6cc8dc691c9696359ba8a
Jassal <TJ081487@cerner.net>
2020-07-06 16:47:27 +0530

    updated javadoc

--- src/com/java/leetcode/julychallenge/OnePlus.java
+++ src/com/java/leetcode/julychallenge/OnePlus.java
@@ -1,37 +1,44 @@
 package com.java.leetcode.julychallenge;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Given a non-empty array of digits representing a non-negative integer, plus
+ * one to the integer. The digits are stored such that the most significant
+ * digit is at the head of the list, and each element in the array contain a
+ * single digit.
+ *
+ */
 public class OnePlus {
 	public static void main(String[] args) {
-		//	int[]cells= {7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
-		int[]cells= {9};
-		Arrays.stream(plusOne(cells)).forEach(a->System.out.print(a));
+		int[] cells = { 7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1,
+				1, 1, 7, 4, 0, 0, 6 };
+		Arrays.stream(plusOne(cells)).forEach(a -> System.out.print(a));
 	}
 
+	/**
+	 * This method returns the array of digits after adding one to the integer. This
+	 * can be achieved by BigInteger as well.
+	 * 
+	 * @param digits array of digits of integer.
+	 * @return
+	 */
 	public static int[] plusOne(int[] digits) {
 		List<Integer> list = new LinkedList<>();
 		int carry = 0;
-		for (int i = digits.length - 1; i >= 0; i--) { 
-			System.out.println("i is :" + i + " carry is :" + carry);
+		for (int i = digits.length - 1; i >= 0; i--) {
 			int num = digits[i] + carry;
-			System.out.println("num1 is :" + num);
 			if (i == digits.length - 1) {
 				num = num + 1;
-				System.out.println("num2 is :" + num);
-
 			}
-			System.out.println("before condition :" + (num / 10));
 			if (num / 10 == 0) {
-				System.out.println("if :" + num);
-
 				carry = 0;
 				list.add(num);
 			} else {
 				carry = 1;
-				System.out.println("else :" + num + "num %10 is :" + (num % 10));
 				list.add(num % 10);
 			}
 		}
@@ -41,5 +48,4 @@ public class OnePlus {
 		Collections.reverse(list);
 		return list.stream().mapToInt(Integer::intValue).toArray();
 	}
-
 }
